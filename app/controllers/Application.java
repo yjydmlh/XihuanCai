419896a13aa622d785646dfa210650f43b5145f5
Roy Guo <royguo1988@gmail.com>
2012-02-03 23:40:23 +0800

    #

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,7 +5,10 @@ import play.mvc.Controller;
 public class Application extends Controller {
 
     public static void index() {
-        render();
+      render();
     }
 
+    public static void about(){
+      render();
+    }
 }
